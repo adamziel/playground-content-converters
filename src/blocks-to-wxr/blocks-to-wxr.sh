e54a398f90fdda45864b18dc8469ae3492a2d7f2
Adam Zieliński <adam@adamziel.com>
2024-06-12 13:36:30 +0200

    Don't import markdown twice

--- src/blocks-to-wxr/blocks-to-wxr.sh
+++ src/blocks-to-wxr/blocks-to-wxr.sh
@@ -7,7 +7,7 @@ OUTPUT_PATH=$2
 SCRIPT_PATH="$(dirname "$(realpath "$0")")"
 
 bunx @wp-playground/cli@latest \
-    run-blueprint \
+    server \
     --mount=$BLOCK_MARKUP_PATH:/block-markup \
     --mount=$OUTPUT_PATH:/output \
     --mount=$SCRIPT_PATH/block-markup-importer:/wordpress/wp-content/plugins/block-markup-importer \
