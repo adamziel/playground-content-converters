cb21b9a1698dcbd33cbaef07e0ff23cf06138009
Adam Zieliński <adam@adamziel.com>
2024-06-15 10:50:11 +0200

    Stuff

--- src/run-markdown-editor.sh
+++ src/run-markdown-editor.sh
@@ -14,4 +14,6 @@ bun ../playground/packages/playground/cli/src/cli.ts \
     --mount=$SCRIPT_PATH/import-static-files:/wordpress/wp-content/plugins/import-static-files \
     --mount=$SCRIPT_PATH/store-markdown-as-post-meta:/wordpress/wp-content/plugins/store-markdown-as-post-meta \
     --mount=$SCRIPT_PATH/save-pages-as-static-files:/wordpress/wp-content/plugins/save-pages-as-static-files \
+    --mount-after-blueprint=$SCRIPT_PATH/builds/wporg-parent-2021/build:/wordpress/wp-content/themes/wporg-parent-2021/build \
+    --mount-after-blueprint=$SCRIPT_PATH/builds/global-header-footer/build:/wordpress/wp-content/mu-plugins/blocks/global-header-footer/build \
     --blueprint=$SCRIPT_PATH/blueprint.json
